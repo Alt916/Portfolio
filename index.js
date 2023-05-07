4bc586a2f178467611e16fc83943ca188727b81f
Benoit GAILLARD <115778881+unknown7349@users.noreply.github.com>
2023-05-07 22:28:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var toggle_btn = document.getElementById('theme-btn');
 var body = document.getElementsByTagName('body')[0];
 var dark_theme_class = 'dark';
-var cadre1 = document.getElementById('cadre1'); // Ajoutez cette ligne
+var cadre1 = document.getElementById('cadre1');
 
 var toogle_title = document.querySelector('.h2');
 var toogle_titleautre = document.querySelector('.h1');
@@ -18,16 +18,18 @@ toggle_btn.addEventListener('click', function () {
     toogle_titleautre.style.color = "";
     toogle_titleautre.style.fontFamily = "";
     toogle_title.style.fontFamily = "";
-    cadre1.style.backgroundImage = 'url("./photoDeProfil.png")'; // Ajoutez cette ligne
-    document.documentElement.classList.remove('flashlight'); // Retirer la classe "flashlight"
+    cadre1.style.backgroundImage = 'url("./photoDeProfil.png")';
+    document.documentElement.classList.remove('flashlight');
+    document.body.style.overflowX = 'auto'; // Activer le scroll horizontal
   } else {
     body.classList.add(dark_theme_class);
     toogle_title.style.color = "rgb(255, 117, 24)";
     toogle_titleautre.style.color = "rgb(255, 117, 24)";
     toogle_title.style.fontFamily = "'Creepster', cursive";
     toogle_titleautre.style.fontFamily = "'Creepster', cursive";
-    cadre1.style.backgroundImage = 'url("./photoDark.png")'; // Ajoutez cette ligne
-    document.documentElement.classList.add('flashlight'); // Ajouter la classe "flashlight"
+    cadre1.style.backgroundImage = 'url("./photoDark.png")';
+    document.documentElement.classList.add('flashlight');
+    document.body.style.overflowX = 'hidden'; // Désactiver le scroll horizontal
   }
 });
 
